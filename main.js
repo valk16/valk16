1f11454e8090ed4ac584ce0ed3719367e8c6e318
valk16 <141749000+valk16@users.noreply.github.com>
2024-05-03 08:00:41 -0300

    index, style e main

---
+++ main.js
@@ -0,0 +1,65 @@
+</section>
+<script src="main.js"></script>
+</body>
+</html>
+const botoes = document.querySelectorAll(".botao");
+const botoes = document.querySelectorAll(".botao");
+console.log(botoes);
+const botoes = document.querySelectorAll(".botao");
+
+for()
+    const botoes = document.querySelectorAll(".botao");
+
+for(let i=0;)
+    const botoes = document.querySelectorAll(".botao");
+
+for(let i=0;i <botoes.length;i++){
+    console.log();
+}
+const botoes = document.querySelectorAll(".botao");
+
+for(let i=0;i <botoes.length;i++){
+    botoes[i].onclick
+    console.log();
+}
+const botoes = document.querySelectorAll(".botao");
+
+for(let i=0;i <botoes.length;i++){
+    botoes[i].onclick = function(){
+        botoes[i].classList.add("ativo");
+    }
+    console.log();
+}
+const botoes = document.querySelectorAll(".botao");
+
+for(let i=0;i <botoes.length;i++){
+    botoes[i].onclick = function(){
+        
+        for(let j=0;j<botoes.length;j++){
+            botoes[j].classList.remove("ativo");
+        }
+        botoes[i].classList.add("ativo");
+    }
+    console.log();
+}
+const botoes = document.querySelectorAll(".botao");
+
+for(let i=0;i <botoes.length;i++){
+    botoes[i].onclick = function(){
+        
+        for(let j=0;j<botoes.length;j++){
+            botoes[j].classList.remove("ativo");
+        }
+        botoes[i].classList.add("ativo");
+    }
+}
+<script src="main.js"></script>
+const botoes = document.querySelectorAll(".botao");
+console.log(botoes)
+for(let i=0; i <  botoes.length; i++){
+    console.log(i);
+}
+botoes[i].onclick = function(){
+    
+    botoes[i].classList.add("ativo");
+}
